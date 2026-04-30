148b736d380e38b1904b364d91c0027c58f5fc93
RulentWave <49258216+RulentWave@users.noreply.github.com>
2026-04-30 16:35:38 -0400

    Change bootstrap command to use stage1

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 PREFIX=/usr/lib/gentoo-prefix
 ARTIFACT="${ARTIFACT:-gentoo-prefix-rap-x86_64-$(date -u +%Y-%m-%d).tar.gz}"
 
-/workspace/bootstrap-prefix-with-root.sh "$PREFIX" noninteractive
+/workspace/bootstrap-prefix-with-root.sh "$PREFIX" stage1
 
 tar --xattrs --acls --absolute-names \
 	-czvf "/workspace/${ARTIFACT}" "$PREFIX"
